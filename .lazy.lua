f410d4b73d4ac7060b7cd570281ed233692138ac
KIM Hyunjae <42675338+hnjae@users.noreply.github.com>
2025-06-08 22:28:26 +0900

    chore: add .lazy.lua

---
+++ .lazy.lua
@@ -0,0 +1,14 @@
+return {
+  {
+    [1] = "conform.nvim",
+    opts = {
+      formatters_by_ft = { nix = {} },
+    },
+  },
+  {
+    [1] = "nvim-lint",
+    opts = {
+      linters_by_ft = { nix = {} },
+    },
+  },
+}
